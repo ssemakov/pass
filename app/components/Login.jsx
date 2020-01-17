e91be305775cad1cd8257b7c38aa7a9ffe55786f
Simon Semakov <simon.semakov@gmail.com>
2020-01-17 17:01:15 -0500

    Login page UI

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,11 +1,62 @@
 // @flow
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import type { RouterHistory, ContextRouter } from 'react-router';
 import Settings from 'electron-settings';
 import isEmpty from 'lodash/isEmpty';
-import PasswordInput from './PasswordInput';
+import {
+  Field,
+  type FieldInputProps,
+  type FormikProps,
+  Formik,
+  Form,
+  ErrorMessage
+} from 'formik';
+import {
+  Container,
+  Grid,
+  InputAdornment,
+  IconButton,
+  Typography,
+  OutlinedInput
+} from '@material-ui/core';
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
 
-type LoginProps = {|
+type FormValues = {
+  password: string
+};
+
+type PasswordFieldProps = {
+  disabled: boolean,
+  field: FieldInputProps<FormValues>
+};
+
+function PasswordField({ disabled, field }: PasswordFieldProps) {
+  return (
+    <OutlinedInput
+      autoFocus
+      fullWidth
+      id="password-input"
+      type="password"
+      placeholder="Enter your Master Password"
+      disabled={disabled}
+      {...field}
+      endAdornment={
+        <InputAdornment position="end" variant="filled">
+          <IconButton
+            aria-label="enter"
+            color="primary"
+            type="submit"
+            disabled={disabled}
+          >
+            <KeyboardReturnIcon />
+          </IconButton>
+        </InputAdornment>
+      }
+    />
+  );
+}
+
+type FormProps = {|
   authenticated: boolean,
   authenticate: (secret: string, path: ?string) => Promise<boolean>,
   storagePath: ?string,
@@ -14,16 +65,14 @@ type LoginProps = {|
   ...ContextRouter
 |};
 
-export default function(props: LoginProps) {
+export default function(props: FormProps) {
   const {
     authenticate,
     authenticated,
+    history,
     storagePath,
-    setStoragePath,
-    history
+    setStoragePath
   } = props;
-  const passwordInput = useRef('');
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const defaultStoragePath = Settings.get('default_storage_path');
@@ -36,14 +85,15 @@ export default function(props: LoginProps) {
     }
   }, [authenticated]);
 
-  const handleUnlockClick = () => {
-    const value = (passwordInput.current && passwordInput.current.value) || '';
-
-    authenticate(value, storagePath).catch(exception => {
+  const handleSubmit = ({ password }, formik) => {
+    authenticate(password, storagePath).catch(exception => {
       if (exception.code === 'SQLITE_NOTADB') {
-        setError("That didn't work");
+        formik.setFieldError('password', "That didn't work");
       } else if (exception.code === 'ENOENT') {
-        setError(`Can't find storage at '${storagePath || './storage.pass'}'`);
+        formik.setFieldError(
+          'password',
+          `Can't find storage at '${storagePath || './storage.pass'}'`
+        );
       } else {
         throw exception;
       }
@@ -51,22 +101,52 @@ export default function(props: LoginProps) {
   };
 
   return (
-    <>
-      <PasswordInput
-        placeholder="Enter your master password"
-        onChange={() => setError(null)}
-        ref={passwordInput}
-      />
-      {error && <div>{error}</div>}
-      <div>
-        <button
-          type="button"
-          disabled={isEmpty(storagePath)}
-          onClick={handleUnlockClick}
-        >
-          Unlock
-        </button>
-      </div>
-    </>
+    <Formik
+      initialValues={{
+        password: ''
+      }}
+      onSubmit={handleSubmit}
+      render={(formik: FormikProps<FormValues>) => {
+        console.log('formik', formik);
+        return (
+          <Form>
+            <Container maxWidth="sm">
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+              >
+                <Grid item style={{ marginTop: 30 }}>
+                  <Field
+                    name="password"
+                    component={PasswordField}
+                    disabled={isEmpty(storagePath)}
+                  />
+                </Grid>
+                <Grid item>
+                  <Grid container justify="center">
+                    <Grid item>
+                      {isEmpty(formik.errors) ? (
+                        <>&nbsp;</>
+                      ) : (
+                        <ErrorMessage
+                          name="password"
+                          render={(message: string) => (
+                            <Typography variant="body2" color="error">
+                              {message}
+                            </Typography>
+                          )}
+                        />
+                      )}
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Container>
+          </Form>
+        );
+      }}
+    />
   );
 }
